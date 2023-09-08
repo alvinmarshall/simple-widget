8c190b235d21eae43a69099a576659c4e6d3c0be
alvinmarshall <kelvinbirikorang@gmail.com>
2023-09-07 22:22:25 -0400

    add webpack configuration
    define babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'widget.js',
+        library: 'RaasWidget',
+        publicPath: '/',
+        libraryExport: 'default',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }]
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            inject: 'head',
+            scriptLoading: 'blocking'
+        }),
+    ],
+};
